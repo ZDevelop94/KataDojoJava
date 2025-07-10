7e9b04e8960871786989695d924f3b134398b1ef
Zephaniah Flynn <18018780+ZDevelop94@users.noreply.github.com>
2025-07-10 05:04:15 +0100

    A additional test for edge scenario

--- src/test/java/GameOfLifeGeneratorTest.java
+++ src/test/java/GameOfLifeGeneratorTest.java
@@ -43,6 +43,23 @@ public class GameOfLifeGeneratorTest {
                         {".",".",".","*","*",".",".",".","."},
                         {".",".",".",".",".",".",".",".","."}
         };
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Generate the next generation from a 4 by 8 board with Alive cells on the edge")
+    void GameOfLifeTest3() {
+        var file = new File("./src/test/resources/generation4by8Edge.txt");
+
+        String[][] result = gameGenerator.nextGeneration(file);
+
+        String[][] expected =
+                new String[][]{
+                        {".",".",".",".",".",".",".","."},
+                        {".",".",".","*","*",".",".","."},
+                        {".",".",".","*","*",".",".","."},
+                        {".",".",".",".",".",".",".","."}
+                };
 
         assertArrayEquals(expected, result);
 
